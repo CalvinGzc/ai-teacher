00dead45d5f3ace6d1fa2b6954b461554f1c2b88
CalvinG <821154006@qq.com>
2024-12-24 10:41:59 +0800

    20241224-4

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  swcMinify: false,
+  experimental: {
+    // 禁用 SWC
+    forceSwcTransforms: false
+  },
+  // 指定 webpack 配置
+  webpack: (config, { isServer }) => {
+    // 保持现有配置
+    return config
+  }
+}
 
 module.exports = nextConfig 
